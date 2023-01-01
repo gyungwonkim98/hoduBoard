8786afe32428812a234a38c7db8dc1672ad7dfa3
gyungwonkim <juykim98@naver.com>
2023-01-01 20:28:22 +0900

    Initial Commit - hoduBoard

---
+++ src/test/java/com/example/hoduBoard/HoduBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoduBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoduBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
